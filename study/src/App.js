ea91233411a47e1ca1c99db7a4ea0d6917a962ef
kimsungkwang <skk405@naver.com>
2022-10-16 16:29:27 +0900

    컴포넌트

--- study/src/App.js
+++ study/src/App.js
@@ -1,10 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+function Header() {
+  return (
+    <headers>
+      <h1>
+        <a href="/">WEB</a>
+      </h1>
+    </headers>
+  );
+}
+
+function Nav() {
+  return (
+    <nav>
+      <li>
+        <a href="/">html</a>
+      </li>
+      <li>
+        <a href="/">css</a>
+      </li>
+      <li>
+        <a href="/">js</a>
+      </li>
+    </nav>
+  );
+}
+
+function Article() {
+  return (
+    <article>
+      <h2>Welcom</h2>
+      Hello, Web
+    </article>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      Hello React !! 
+    <div>
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
     </div>
   );
 }
